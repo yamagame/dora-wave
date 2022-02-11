1d0aaeaffc66be1639bd01b9fc0adcd1f0aaf0e2
yamagame <yamagame@mac.com>
2022-02-11 19:24:21 +0900

    メンテナンス

--- copy-to-dora.sh
+++ copy-to-dora.sh
@@ -4,5 +4,5 @@ rm ../dora-engine/public/wave-analyzer/index.html
 rm ../dora-engine/public/static/js/main-wave-analyzer.*.js
 rm ../dora-engine/public/static/css/main-wave-analyzer.*.css
 cp ./build/index.html ../dora-engine/public/wave-analyzer/
-cp ./build/static/js/main-*.js ../dora-engine/public/static/js/
-cp ./build/static/css/main-*.css ../dora-engine/public/static/css/
+cp ./build/static/js/main.*.js ../dora-engine/public/static/js/
+cp ./build/static/css/main.*.css ../dora-engine/public/static/css/
